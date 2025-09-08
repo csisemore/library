3663ac69444adc892f27d6e58ee3d38b49a83ab5
csisemore <camilla.sisemore@gmail.com>
2025-09-08 11:06:45 -0500

    Adding new files

---
+++ test.py
@@ -0,0 +1,29 @@
+import sqlite3
+con = sqlite3.connect("tutorial.db")
+
+cur = con.cursor()
+
+cur.execute("CREATE TABLE movie(title, year, score)")
+
+res = cur.execute("SELECT name FROM sqlite_master")
+res.fetchone()
+
+res = cur.execute("SELECT name FROM sqlite_master WHERE name='spam'")
+res.fetchone() is None
+
+cur.execute("""
+    INSERT INTO movie VALUES
+        ('Monty Python and the Holy Grail', 1975, 8.2),
+        ('And Now for Something Completely Different', 1971, 7.5)
+""")
+
+res = cur.execute("SELECT score FROM movie")
+res.fetchall()
+
+data = [
+    ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
+    ("Monty Python's The Meaning of Life", 1983, 7.5),
+    ("Monty Python's Life of Brian", 1979, 8.0),
+]
+cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
+con.commit()  # Remember to commit the transaction after executing INSERT.
